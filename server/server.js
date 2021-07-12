44459eb54a295dc2aa5489235bd600fc5d870a57
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-12 13:27:23 +0300

    import routers into server/server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const connectDB = require('./database/connection');
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const loginRoute = require('./routes/login');
+const registerRoute = require('./routes/register');
+app.use('/', loginRoute, registerRoute);
 
 const Port = process.env.Port || 3000;
 app.listen(Port, () => console.log('Server started..!'));
