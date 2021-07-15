0ffd6ede4b19251e9a052a66a251f67583f4ae51
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-16 01:30:38 +0300

    import app.js to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './css/main.css';
+
+import App from './scripts/app';
+
+let app = new App();
+
+app.init();
