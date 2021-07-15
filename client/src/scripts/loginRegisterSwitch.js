9e0927d3c300f7d4ed0df7bc9c2fb6efa563ac58
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-16 01:33:26 +0300

    add login to register and register to login switch

---
+++ client/src/scripts/loginRegisterSwitch.js
@@ -0,0 +1,33 @@
+export const showSignInForm = () => {
+  const signUpButton = document.querySelector('#signup-btn');
+  signUpButton.classList.add('click-button');
+
+  const signInButton = document.querySelector('#signin-btn');
+  signInButton.classList.remove('click-button');
+
+  const signInDiv = document.querySelector('#login-element');
+  signInDiv.style.visibility = 'visible';
+
+  const signUpDiv = document.querySelector('#register-element');
+  signUpDiv.style.visibility = 'hidden';
+
+  const logRegFrame = document.querySelector('#log-reg-frame');
+  logRegFrame.style.height = '340px';
+};
+
+export const showSignUpForm = () => {
+  const signInButton = document.querySelector('#signin-btn');
+  signInButton.classList.add('click-button');
+
+  const signUpButton = document.querySelector('#signup-btn');
+  signUpButton.classList.remove('click-button');
+
+  const signInDiv = document.querySelector('#login-element');
+  signInDiv.style.visibility = 'hidden';
+
+  const signUpDiv = document.querySelector('#register-element');
+  signUpDiv.style.visibility = 'visible';
+
+  const logRegFrame = document.querySelector('#log-reg-frame');
+  logRegFrame.style.height = '500px';
+};
