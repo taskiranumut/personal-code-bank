f41e75e71da9f4aca906a64fecccb554a35f146b
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-16 01:21:27 +0300

    refactor get to post

--- server/routes/blockGet.js
+++ server/routes/blockGet.js
@@ -3,7 +3,7 @@ const Block = require('../database/models/Block');
 
 const router = express.Router();
 
-router.get('/user-blocks', async (req, res) => {
+router.post('/user-blocks', async (req, res) => {
   console.log(req.body);
   try {
     const blocks = await Block.find(req.body);
