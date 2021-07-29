631619dbe6bf296ead7ee31b3b98d84fe10b91c5
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-30 02:36:08 +0300

    add post functions (login, register, userblock)  into fetch/post.js

---
+++ client/src/scripts/fetch/post.js
@@ -0,0 +1,37 @@
+import { API_URL } from './API_URL';
+
+export const registerToDatabase = (userRegisterInfos) => {
+  const { firstName, lastName, email, password } = userRegisterInfos;
+  return fetch(`${API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+};
+
+export const confirmUserInDatabase = (userLoginInfos) => {
+  const { email, password } = userLoginInfos;
+  return fetch(`${API_URL}/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+};
+
+export const getUserBlocksFromDatabase = (userId) => {
+  return fetch(`${API_URL}/user-blocks`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user: userId,
+    }),
+  });
+};
