13101db259098a4c3eb4a937e77178421b8a31f2
umuttaskiran <umut.taskiran7@gmail.com>
2021-07-31 02:10:55 +0300

    move localstorage functions to helpers.js

--- client/src/scripts/helpers.js
+++ client/src/scripts/helpers.js
@@ -1,7 +1,27 @@
+export const getUserIdFromLocalStorage = () => {
+  const result = localStorage.getItem('token');
+  const userDataRaw = result.split('.')[1];
+  const userData = atob(userDataRaw);
+  const objectUserData = JSON.parse(userData);
+  return objectUserData.id;
+};
+
 export const clearMessageDiv = () => {
   getMessageDivEl().innerHTML = '';
 };
 
 export const getMessageDivEl = () => {
   return document.querySelector('#message');
+};
+
+export const clearClickTagClass = () => {
+  const $clickTagEl = document.querySelector('.click-tag');
+  if ($clickTagEl) {
+    $clickTagEl.classList.remove('click-tag');
+  }
+};
+
+export const addClickTagClass = (blockTag) => {
+  const $tagEl = document.getElementById(blockTag);
+  $tagEl.classList.add('click-tag');
 };
